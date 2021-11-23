ad78d32377c0cdac256528b575d6218da763d676
Ritik Kaushik <ritik.kaushik@crimsonbeans.co.in>
2021-11-23 15:01:48 +0530

    COde fixes

--- src/components/PaymentSuccessDialog.js
+++ src/components/PaymentSuccessDialog.js
@@ -10,43 +10,6 @@ function PaymentSuccessDialog(props) {
   const classes = materialStyles();
   let navigate = useNavigate();
 
-  const CList = ({ title, text, bold = false }) => {
-    return (
-      <div
-        style={{
-          display: "flex",
-          paddingLeft: "30px",
-          paddingRight: "30px",
-          marginTop: "10px",
-        }}
-      >
-        <div style={{ flex: 1, display: "flex", justifyContent: "flex-start" }}>
-          <Typography
-            style={{
-              fontSize: bold ? "18px" : "16px",
-              fontWeight: bold ? "600" : "400",
-              opacity: 0.8,
-              lineHeight: "30px",
-            }}
-          >
-            {title}
-          </Typography>
-        </div>
-        <div style={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
-          <Typography
-            style={{
-              fontSize: bold ? "18px" : "16px",
-              fontWeight: bold ? "600" : "400",
-              opacity: 0.8,
-              lineHeight: "30px",
-            }}
-          >
-            {text}
-          </Typography>
-        </div>
-      </div>
-    );
-  };
   return (
     <div style={{ height: "20vh" }}>
       <div
@@ -77,22 +40,6 @@ function PaymentSuccessDialog(props) {
           Successful
         </Typography>
       </div>
-      {/* <CList title={"Payment Type"} text={"Card"} />
-      <CList title={"Mobile"} text={"0123456789"} />
-      <CList title={"Email"} text={"xyz@email.com"} />
-      <CList title={"Transaction id"} text={"12345678946584"} />
-      <CList title={"Amount paid"} text={"$47.00"} bold /> */}
-      {/* <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          marginTop: "20px",
-        }}
-      >
-        <Button variant="text" onClick={() => navigate("/")}>
-          Go Back
-        </Button>
-      </div> */}
     </div>
   );
 }
